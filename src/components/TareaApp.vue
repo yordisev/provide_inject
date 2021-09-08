bc445aea55b11f028ac33b6193f65e5bec068f5c
yordisev <yordys12@gmail.com>
2021-09-08 16:19:26 -0500

    TareaItem

--- src/components/TareaApp.vue
+++ src/components/TareaApp.vue
@@ -2,14 +2,23 @@
 <h1>App Tareas</h1>
 <tarea-form/>
   {{tareas}}
+  <tarea-item
+  v-for="tarea in tareas" :key="tarea.id"
+  :tarea="tarea"
+  />
+
+  <div class="alert alert-dark mt-3" v-if="tareas.length === 0">
+    sin tareas pendientes 😁
+  </div>
 </template>
 
 <script>
 import {ref } from '@vue/reactivity'
 import TareaForm from './TareaForm.vue'
 import { provide } from '@vue/runtime-core'
+import TareaItem from './TareaItem.vue'
 export default {
-  components: { TareaForm },
+  components: { TareaForm, TareaItem },
 setup(){
     const tareas = ref([])
 
--- src/components/TareaItem.vue
+++ src/components/TareaItem.vue
@@ -1,10 +1,21 @@
 <template>
-  Item secundarios
+  <div class="alert alert-warning mt-3 d-flex justify-content-between align-items-center">
+    <p class="m-0">{{tarea.texto}}</p>
+    <div>
+      <i class="fas fa-check-circle mx-2 text-success" role="button"></i>
+      <i class="fas fa-minus-circle text-danger"></i>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+props:{
+  tarea:{
+    type:Object,
+    required:true
+  }
+}
 }
 </script>
 
