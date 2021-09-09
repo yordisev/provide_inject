c32e721d100dbe55125d8f67c0180592b198d3f0
yordisev <yordys12@gmail.com>
2021-09-08 19:53:47 -0500

    portatil

--- src/components/TareaItem.vue
+++ src/components/TareaItem.vue
@@ -2,20 +2,46 @@
   <div class="alert alert-warning mt-3 d-flex justify-content-between align-items-center">
     <p class="m-0">{{tarea.texto}}</p>
     <div>
-      <i class="fas fa-check-circle mx-2 text-success" role="button"></i>
-      <i class="fas fa-minus-circle text-danger"></i>
+<i class="fas fa-undo-alt mx-2 text-success" role="button"
+      @click="modificar(tarea.id)"
+      ></i>
+
+      <i class="fas fa-check-circle mx-2 text-success" role="button"
+      @click="modificar(tarea.id)"
+      ></i>
+      <i class="fas fa-minus-circle text-danger"
+      @click="eliminar(tarea.id)"
+      ></i>
     </div>
   </div>
 </template>
 
 <script>
+import { inject } from '@vue/runtime-core'
 export default {
 props:{
   tarea:{
     type:Object,
     required:true
   }
-}
+},
+setup(){
+    const tareas = inject('tareas')
+
+    const eliminar = id => {
+      tareas.value = tareas.value.filter(item => item.id !== id)
+    }
+  const modificar = id => {
+    tareas.value = tareas.value.map(item => {
+      if (item.id === id){
+        item.estado = !item.estado
+      }
+      return item
+    })
+  }
+  return {eliminar, modificar}
+  }
+
 }
 </script>
 
